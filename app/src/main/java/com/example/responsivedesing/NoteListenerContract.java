dbc3498a44f4cb7151df26aaf66e5edee7654832
kamaxo93 <kamaxo93@gmail.com>
2021-02-28 19:14:43 +0100

    initialization Room

--- app/src/main/java/com/example/responsivedesing/NoteListenerContract.java
+++
@@ -1,7 +0,0 @@
-package com.example.responsivedesing;
-
-public interface NoteListenerContract {
-    void editNoteClick(Note note);
-    void deleteNoteClick(Note note);
-    void favoriteNoteClick(Note note);
-}
